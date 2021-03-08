67af871ce2761415f8c74abaa6c91a406faab0c8
Romario Gustave <romagustave10@gmail.com>
2021-03-08 02:36:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+  
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+   word => word[0].toUpperCase() + word.slice(1)
+).join(' ')
 }
